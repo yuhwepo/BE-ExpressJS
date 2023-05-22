8e396c2bb8f4a4e871c95b05f02619bdc6918da7
Nguyen Van Minh Huy <yuhwepo@gmail.com>
2023-05-22 19:22:57 +0700

    forgot-reset-password

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const jsonwebtoken = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const db = require("../database/connection");
 const { hashwithRandomSalt, comparePassword } = require("../middlewares/hash");
@@ -8,7 +9,15 @@ const {
 	validateUser,
 	validateRegisterRequest,
 } = require("../middlewares/validate");
-const { executeQuery, getOne, create } = require("../database/query");
+const {
+	getOne,
+	getMany,
+	executeQuery,
+	updateOne,
+	create,
+	deleteOne,
+} = require("../database/query");
+const { mailService } = require("../services/mail.service");
 
 const secret = process.env.JWT_SECRET;
 
@@ -86,5 +95,96 @@ authRouter.post(
 		return res.status(201).json({ message: "User created successfully" });
 	}
 );
+
+// authRouter.post("/reset-password", async function (req, res) {
+// 	const { email } = req.body;
+
+// 	await mailService.sendEmail({
+// 		emailFrom: "yuhwepo@gmail.com",
+// 		emailTo: email,
+// 		emailSubject: "Reset Password",
+// 		emailText: "Text....",
+// 	});
+// 	return res.status(200).json({ message: "Reset Password" });
+// });
+
+authRouter.post("/forgot-password", async (req, res) => {
+	try {
+		const { email } = req.body;
+
+		const user = await getOne({
+			db,
+			query: "SELECT * FROM users WHERE email = ?",
+			params: [email],
+		});
+
+		if (!user) {
+			return res.status(404).json({ message: "Email not found" });
+		}
+
+		const secretKey = crypto.randomBytes(32).toString("hex");
+		const passwordResetToken = crypto
+			.createHash("sha256")
+			.update(secretKey)
+			.digest("hex");
+
+		const passwordResetAt = new Date(Date.now() + 10 * 60 * 1000);
+		const updateStatus = await updateOne({
+			db,
+			query: "UPDATE users SET passwordResetToken = ?, passwordResetAt = ? WHERE email = ?",
+			params: [passwordResetToken, passwordResetAt, email],
+		});
+
+		if (updateStatus) {
+			mailService.sendEmail({
+				emailFrom: "admin@gmail.com",
+				emailTo: email,
+				emailSubject: "Reset Password",
+				emailText: `Here is your reset password token: ${passwordResetToken}`,
+			});
+			return res
+				.status(200)
+				.json({ message: "Reset Password email sent successfully" });
+		}
+		return res.status(400).json({ message: "Can not reset password" });
+	} catch (err) {
+		return res.status(500).json({ message: err.message });
+	}
+});
+
+authRouter.post("/reset-password", async (req, res) => {
+	try {
+		const { email, passwordResetToken, newPassword } = req.body;
+
+		const user = await getOne({
+			db,
+			query: "SELECT * FROM users WHERE email = ? AND passwordResetToken = ? AND passwordResetAt = ?",
+			params: [email, passwordResetToken, new Date()],
+		});
+
+		if (!user) {
+			return res
+				.status(403)
+				.json({ message: "Invalid token or token has expired" });
+		}
+
+		const { hashedPassword, salt } = await hashwithRandomSalt(newPassword);
+
+		const updateStatus = await updateOne({
+			db,
+			query: "UPDATE users SET password = ?, salt = ?, passwordResetToken = null, passwordResetAt = null WHERE email = ?",
+			params: [hashedPassword, salt, email],
+		});
+
+		if (updateStatus) {
+			return res
+				.status(200)
+				.json({ message: "Reset password successfully" });
+		}
+		return res.status(401).json({ message: "reset password failed" });
+	} catch (err) {
+		return res.status(500).json({ message: err.message });
+	}
+});
 
 module.exports = authRouter;
