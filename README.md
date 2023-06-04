8edefdb0a3248548a48f9313c621df21cae8b835
Nguyen Van Minh Huy <yuhwepo@gmail.com>
2023-06-05 01:15:22 +0700

    fix create db

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # BE-ExpressJS
+
+## Install
+
+-   `npm install`
+-   Create .env to config server and Database (check .env_example)
+
+## Init database
+
+-   `npm run init`
+
+## Start server
+
+-   `npm start`
--- src/database/init.js
+++ src/database/init.js
@@ -1,25 +1,33 @@
-// const connection = require("./connection");
 const connection = require("./knex-connection");
 
-await connection.schema
-	.createTableIfNotExists("users", (table) => {
-		table.increments("id").primary();
-		table.string("username", 50).unique();
-		table.string("password", 255);
-		table.string("salt", 255);
-		table.string("fullname", 255).notNullable();
-		table.boolean("gender");
-		table.string("email", 255);
-		table.interger("age").unsigned();
-		table.string("passwordResetToken", 255);
-		table.dateTime("passwordResetAt");
-		table.timestamp("createdAt").defaultTo(connection.fn.now());
-		table.integer("createdBy");
-		table.boolean("isAdmin").defaultTo(false);
-	})
-	.then(() => {
-		console.log("Table created successfully!");
-	})
-	.catch((err) => {
-		console.error("Error creating table:", err);
-	});
+async function createUsersTable() {
+	try {
+		const tableExists = await connection.schema.hasTable("users");
+		if (!tableExists) {
+			await connection.schema.createTable("users", (table) => {
+				table.increments("id").primary();
+				table.string("username", 50).unique();
+				table.string("password", 255);
+				table.string("salt", 255);
+				table.string("fullname", 255).notNullable();
+				table.boolean("gender");
+				table.string("email", 255);
+				table.integer("age").unsigned();
+				table.string("passwordResetToken", 255);
+				table.dateTime("passwordResetAt");
+				table.timestamp("createdAt").defaultTo(connection.fn.now());
+				table.integer("createdBy").unsigned();
+				table.foreign("createdBy").references("id").inTable("users");
+				table.boolean("isAdmin").defaultTo(false);
+			});
+
+			console.log("Table created successfully!");
+		} else {
+			console.log("Table already exists!");
+		}
+	} catch (error) {
+		console.error("Error creating table:", error);
+	}
+}
+
+createUsersTable();
