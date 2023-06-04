18c854c8fbb51e8f49bac52625032180f1c8792a
Nguyen Van Minh Huy <yuhwepo@gmail.com>
2023-06-05 00:28:36 +0700

    CRUD, pagination and Search

--- src/database/init.js
+++ src/database/init.js
@@ -1,25 +1,44 @@
-const connection = require("./connection");
+// const connection = require("./connection");
+const connection = require("./knex-connection");
 
-const createTableUsers = `
-    CREATE TABLE IF NOT EXISTS users (
-        id INT PRIMARY KEY AUTO_INCREMENT,
-		username VARCHAR(50) UNIQUE,
-		password VARCHAR(255),
-		salt VARCHAR(255),
-        fullname VARCHAR(255) NOT NULL,
-        gender BOOLEAN,
-		email VARCHAR(255),
-        age INT unsigned
-)
-`;
+// const createTableUsers = `
+//     CREATE TABLE IF NOT EXISTS users (
+//         id INT PRIMARY KEY AUTO_INCREMENT,
+// 		username VARCHAR(50) UNIQUE,
+// 		password VARCHAR(255),
+// 		salt VARCHAR(255),
+//         fullname VARCHAR(255) NOT NULL,
+//         gender BOOLEAN,
+// 		email VARCHAR(255),
+//         age INT unsigned
+// )
+// `;
 
-connection.query(createTableUsers, (err, results) => {
-	if (err) {
+await connection.schema
+	.createTableIfNotExists("users", (table) => {
+		table.increments("id").primary();
+		table.string("username", 50).unique();
+		table.string("password", 255);
+		table.string("salt", 255);
+		table.string("fullname", 255).notNullable();
+		table.boolean("gender");
+		table.string("email", 255);
+		table.interger("age").unsigned();
+	})
+	.then(() => {
+		console.log("Table created successfully!");
+	})
+	.catch((err) => {
 		console.error("Error creating table:", err);
-		return;
-	}
-	console.log("Table created successfully!");
-});
+	});
+
+// connection.query(createTableUsers, (err, results) => {
+// 	if (err) {
+// 		console.error("Error creating table:", err);
+// 		return;
+// 	}
+// 	console.log("Table created successfully!");
+// });
 
 // const insertData = `
 //     INSERT INTO users (username, password, salt, fullname, gender, email, age) VALUES
